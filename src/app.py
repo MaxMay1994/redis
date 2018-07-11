f6709c4b0505e8b04d4b3aa3c2b6e6fa2064343d
maxma <max.may@live.de>
2018-07-11 09:05:21 +0200

    added app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,35 @@
-import redis
 from flask import Flask
+from flask import request
+import redis
 
+r_server = redis.Redis('localhost')
+r_server.set('user:next', '1')
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'INDEX'
+
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+        return r_server.get('user')
+
+@app.route('/users/<string:id>', methods=['GET'])
+def getUsersId(id):
+        return r_server.get('user:'+id)
+
+
+@app.route('/users', methods=['POST'])
+def postUsers():
+        userName = request.form['name']
+        userKey = 'user:' + r_server.get('user:next').decode('utf-8')
+        r_server.set(userKey, userName)
+        r_server.incr('user:next')
+        return userName
+
+
+if __name__ == '__main__':
+    app.run()
